437d84386f9472c7f7c4ea611fc276f8454a75b7
west <west@tradev-group.com>
2023-11-21 19:07:57 +0800

    add payin and payout

---
+++ Constant.py
@@ -0,0 +1,20 @@
+CURRENCY = "IDR"
+F_0 = "yyyy-MM-dd'T'HH:mm:ssXXX"
+
+# from merchant platform: merchantId
+MERCHANT_ID = "sandbox-10004"
+
+# from merchant platform: merchantId
+MERCHANT_SECRET = "4ee78294246e003ebc9c3fdab013b9003a9eed7aaff5b2c433dbf54e95e00550"
+
+# from docs API
+BASE_URL = "https://sandbox-gateway-test.smilepay.id"
+
+# from docs API
+ACCESS_TOKEN_API = "/v1.0/access-token/b2b"
+
+# from docs API
+PAY_IN_API = "/v1.0/transaction/pay-in"
+
+# from docs API
+PAY_OUT_API = "/v1.0/disbursement/cash-out"
